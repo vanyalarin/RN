e77311968e384d85b62e10aba1ae40c851787756
vanyalarin <vanyalarin2000@gmail.com>
2020-07-27 19:30:47 +0300

    rendering of List

--- react-native-demo/App.js
+++ react-native-demo/App.js
@@ -1,16 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {Navbar} from './src/Navbar'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
+import { Navbar } from './src/Navbar'
+import { AddTodo } from './src/AddTodo'
+import { Todo } from './src/Todo'
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, title: 'test' },
+    { id: 2, title: 'test' },
+    { id: 3, title: 'test' },
+    { id: 4, title: 'test' },
+    { id: 5, title: 'test' },
+    { id: 6, title: 'test' },
+    { id: 7, title: 'test' }
+  ])
+
+  const addTodo = title => {
+    setTodos(prev => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title
+      }
+    ])
+  }
+
   return (
-    <View style={styles.container}>
-      <Navbar title='Todo App' /> 
+    <View>
+      <Navbar title='Todo App!' />
+      <View style={styles.container}>
+        <AddTodo onSubmit={addTodo} />
+
+        <FlatList
+          keyExtractor={item => item.id.toString()}
+          data={todos}
+          renderItem={({ item }) => <Todo todo={item} />}
+        />
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {}
+  container: {
+    paddingHorizontal: 30,
+    paddingVertical: 20
+  }
 })
